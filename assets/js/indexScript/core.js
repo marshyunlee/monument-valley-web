c4bff06561a48fae4be7bfa89e75c56fdc296140
Marshall <gkslql456@gmail.com>
2023-03-09 23:27:13 -0800

    testing

--- assets/js/indexScript/core.js
+++ assets/js/indexScript/core.js
@@ -11,12 +11,12 @@ $(function() {
 	resizeContainers();
 
 	// Check for WebGL
-	if (!window.WebGLRenderingContext)
-	{
-		// the browser doesn't even know what WebGL is
-		initPageNoWebGL();
-	} else
-	{
+	// if (!window.WebGLRenderingContext)
+	// {
+	// 	// the browser doesn't even know what WebGL is
+	// 	initPageNoWebGL();
+	// } else
+	// {
 		var canvas = document.getElementById("canvas");
     	var ctx = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
     	if (!ctx)
@@ -28,7 +28,7 @@ $(function() {
 			initPage();
 			init3DText();
 		}
-	}
+	// }
 
   	// Initialise scroller library
 	var s = skrollr.init({
